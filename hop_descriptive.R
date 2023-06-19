5dc21556598aebc89a70aae453320d78d508d37c
deborahfnoliveira <48049004+deborahfnoliveira@users.noreply.github.com>
2023-06-19 10:36:59 +0200

    Add just by country

--- hop_descriptive.R
+++ hop_descriptive.R
@@ -15,6 +15,19 @@ head(hop_aromas)
 paste("There are", length(hop_aromas$hop_name), "hops in this database. Information was retrieved from www.beermaverick.com on June 17, 2023.")
 
 # where are they from and what is their purpose
+# just countries
+hop_aromas %>% 
+  count(country) %>%
+  arrange(desc(n)) %>%
+  mutate(country_code = countrycode::countrycode(country, 'country.name', 'genc2c')) %>% 
+  gt() %>%
+  cols_move_to_start(country_code) %>% 
+  fmt_flag(columns = country_code) %>% 
+  cols_label(country_code = '',
+             country = md('**Country**'),
+             n = md('**Hop strains**'))
+
+# countries + purpose
 hop_aromas %>% 
   group_by(country) %>%
   mutate(n_hops = n()) %>%
