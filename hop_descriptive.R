7eae9eed70feba778e0073bdffb274e2b5ddce5c
VolundurH <volundurh@gmail.com>
2023-06-20 14:22:08 +0200

    add descriptive plots

--- hop_descriptive.R
+++ hop_descriptive.R
@@ -39,5 +39,83 @@ hop_aromas %>%
 
 
 
+# Radial barplot to display aroma profiles 
+
+radial_barplot <- function(hop){
+  
+  hop_aromas |> 
+    filter(hop_name == hop) |> 
+    pivot_longer(
+      cols = -c(hop_name, hop_purpose, country, link, aroma_tags, country_code),
+      names_to = "aroma", 
+      values_to = "aroma_value"
+    ) |> 
+    mutate(aroma = str_replace(aroma, "(?<=[:lower:])(?=[:upper:])", "\n"))|>
+    ggplot(aes(x = fct_inorder(aroma), y = aroma_value, group = hop_name)) +
+    geom_col(aes(fill = aroma), show.legend = F) +
+    geom_segment(aes(xend = fct_inorder(aroma), y = 5, yend  = 0), linetype = 2, alpha = 0.5) +
+    coord_polar() +
+    theme_minimal() + 
+    theme(legend.position = "none",
+      axis.text.y = element_blank(),
+      # panel.border = element_rect(fill = NA),
+      plot.margin = margin(0,0,0,0,"mm"),
+      plot.background = element_rect(fill = NA, colour = NA), 
+      panel.background =  element_rect(fill = NA, colour = NA), 
+      panel.border =  element_rect(fill = NA, colour = NA),
+      axis.line.x = element_blank()) +
+    labs(x = NULL, y = NULL) +
+    scale_y_continuous(limits = c(0,5)) +
+    annotate("text", x = 0, y = c(0:5), label= c(0:5)) + 
+    scale_fill_viridis_d()
+}
 
 
+radial_barplot("Citra")  
+radial_barplot("Galaxy")  
+radial_barplot("Styrian Wolf")  
+
+
+# Summary plots for a list of hops
+
+example_hops <- hop_aromas |> 
+  filter(hop_name %in% c("Citra", "Galaxy", "Styrian Wolf"))
+
+hop_aromas |> 
+  select(hop_name, Citrus:Pine) |> 
+  drop_na() |> 
+  slice_sample(n = 10) |> 
+  pivot_longer(cols = -hop_name, names_to = "aroma", values_to = "value") |> 
+  mutate(aroma  = str_replace(aroma, "Fruit", "\nFruit")) |> 
+  ggplot(aes(x = fct_inorder(aroma), y = value)) + 
+  geom_boxplot(aes(fill = aroma), show.legend = F) +
+  theme_classic() +
+  labs(x = NULL, y = "Aroma intensity") +
+  scale_y_continuous(limits = c(0,5)) +
+  scale_fill_viridis_d()
+
+
+# Oil breakdown plot
+
+hop_oils_plot_df <-hop_brew_values |> 
+  filter(brew_value %in% c("All Others", "Myrcene", "Humulene", "Farnesene", "Caryophyllene")) |> 
+  left_join(
+    hop_brew_values |> 
+      filter(brew_value == "Total Oils (mL/100g)") |> 
+      select(hop_name, brew_value, range_mean) |> 
+      pivot_wider(names_from = brew_value, values_from = range_mean )
+  ) |> 
+  rename(total_oil = `Total Oils (mL/100g)`) |> 
+  mutate(brew_value = fct_relevel(brew_value, "Farnesene", "Caryophyllene","Myrcene", "Humulene", "All Others"))
+
+hop_oils_plot_df |> 
+  mutate(range_mean = range_mean/100) |> 
+  ggplot(aes(x = fct_inorder(brew_value), y = range_mean, group = hop_name)) +
+  geom_point() +
+  geom_path(alpha = 0.5, aes(col = total_oil)) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(x = "Total oil breakdown", y = NULL)  + 
+  theme_classic()
+
+test
+  plotly::ggplotly()
